17d328451f120a8e73299912d9024bde48140189
Niharika Ray <nikki.ray@airbnb.com>
2018-03-27 17:49:31 -0700

    [Print] Change formatting of notes

--- todolist/screen_printer.go
+++ todolist/screen_printer.go
@@ -39,7 +39,7 @@ func (f *ScreenPrinter) Print(groupedTodos *GroupedTodos, printNotes bool) {
 			f.printTodo(todo)
 			if printNotes {
 				for nid, note := range todo.Notes {
-					fmt.Fprintf(f.Writer, "   %s\t%s\t\n",
+					fmt.Fprintf(f.Writer, "  \t\t\t--> %s %s\t\n",
 						cyan(strconv.Itoa(nid)), note)
 				}
 			}
